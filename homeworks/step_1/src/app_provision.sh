9725468e51bad45a192e03e1c17d8ca054555780
root <root@192.168.0.100>
2025-05-20 10:46:16 +0000

    step changes

--- homeworks/step_1/src/app_provision.sh
+++ homeworks/step_1/src/app_provision.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
+
 sudo apt-get update -y
 sudo apt-get install -y openjdk-11-jdk git maven mysql-server
 
 sudo useradd -m -s /bin/bash app_user
 sudo passwd -d app_user
 
-sudo -u app_user git clone https://github.com/Pavlo-1992/devops_9_pavlo.git /home/app_user/project_dir
-chown app_user /home/app_user/project_dir
+echo "DB_USER=${DB_USER}, DB_PASS=${DB_PASS}, DB_NAME=${DB_NAME}"
 
-su - app_user
+sudo -u app_user git clone https://github.com/Pavlo-1992/devops_9_pavlo.git "$PROJECT_DIR"
 
-cd /home/app_user/project_dir/homeworks/step_1/petclinic
-chmod +x mvnw
-./mvnw package
-java -jar target/*.jar
+cd "$PROJECT_DIR/homeworks/step_1/petclinic" || exit 1
+sudo -u app_user mvn clean package
+cp target/*.jar "$PROJECT_DIR/homeworks/step_1/petclinic/web.jar"
+
+
+sudo -u app_user nohup java \
+  -DMYSQL_URL="jdbc:mysql://${DB_HOST}/${DB_NAME}" \
+  -DMYSQL_USER="$DB_USER" \
+  -DMYSQL_PASS="$DB_PASS" \
+  -Dspring.profiles.active=mysql \
+  -jar "$PROJECT_DIR/homeworks/step_1/petclinic/web.jar" >/dev/null 2>&1 &
--- homeworks/step_1/src/db_provision.sh
+++ homeworks/step_1/src/db_provision.sh
@@ -5,7 +5,8 @@ sudo apt-get install -y mysql-server
 sudo sed -i "s/bind-address.*/bind-address = 192.168.56.10/" /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo systemctl restart mysql
 
-original version
+echo "DB_USER=${DB_USER}, DB_PASS=${DB_PASS}, DB_NAME=${DB_NAME}"
+
 sudo mysql -u root <<EOF
 CREATE USER '${DB_USER}'@'192.168.56.%' IDENTIFIED BY '${DB_PASS}';
 CREATE DATABASE ${DB_NAME};
