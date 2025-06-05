7ad2f06c3b46b44a3f77f739558706abac722992
root <root@192.168.0.100>
2025-06-05 22:10:33 +0000

    hw_10

---
+++ homeworks/homework_10/src/number.py
@@ -0,0 +1,24 @@
+import random
+random_number = random.randint(1, 100)
+counter = 5
+
+while counter > 0:
+   try:
+       number = int(input("Enter a number:"))  
+       if number == random_number:
+        print("Congratulations! You guessed the right number.")
+        break
+       elif number > random_number:
+        print("Too high!")
+       if number < random_number:
+        print("Too low!")
+
+       counter -= 1
+       print(f"Attempts left: {counter}")
+   except ValueError:
+       print("Invalid input! Please enter an integer.")
+
+if counter == 0:
+    print(f"Sorry, you've run out of attempts. The correct number was {random_number}.")
+
+
